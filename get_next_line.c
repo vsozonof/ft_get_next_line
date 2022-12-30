a97ad588ebb43d978b06df0334a64f78da83c208
bravedinosaur <vr.sozonoff@gmail.com>
2022-12-30 17:52:15 +0100

    ca fonctionne et je sais pas pourquoi

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: vsozonof <vsozonof@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/02 14:02:00 by vsozonof          #+#    #+#             */
-/*   Updated: 2022/12/26 16:56:18 by vsozonof         ###   ########.fr       */
+/*   Updated: 2022/12/30 17:47:29 by vsozonof         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,11 @@ char	*get_next_line(int fd)
 	char			*line;
 	static char		*stash;
 
-	line = NULL;
-	if (BUFFER_SIZE == 0 || fd < 0)
+	if (BUFFER_SIZE <= 0 || fd < 0)
 		return (NULL);
 	stash = read_and_fill_stash(fd, stash);
 	line = get_line(stash);
 	stash = extract_from_stash(stash);
-	if (!line)
-		return (NULL);
 	return (line);
 }
 
@@ -34,13 +31,11 @@ char	*read_and_fill_stash(int fd, char *stash)
 	int			counter;
 
 	counter = 1;
-	while (ft_find_newline(stash) == 0 && counter > 0)
+	while (ft_find_newline(stash) == 0 && counter != 0)
 	{
 		counter = read(fd, tmp, BUFFER_SIZE);
 		tmp[counter] = '\0';
 		stash = free_and_join_stash(stash, tmp);
-		if (!stash || stash[0] == '\0')
-			return (NULL);
 	}
 	return (stash);
 }
@@ -68,15 +63,9 @@ char	*get_line(char *stash)
 {
 	char	*new_line;
 
-	if (ft_find_newline(stash) == 0 && stash[ft_strlen(stash)] != '\n')
-	{	
-		new_line = ft_substr(stash, 0, ft_strlen(stash));
-		// free(stash);
-	}
-	else if (ft_find_newline(stash) > 0)
-		new_line = ft_substr(stash, 0, ft_find_newline(stash));
-	else
+	if (!stash)
 		return (NULL);
+	new_line = ft_substr(stash, 0, ft_find_newline(stash));
 	return (new_line);
 }
 
@@ -86,36 +75,50 @@ int	main(void)
 	char	*str;
 
 	fd = open("test", O_RDWR);
-	str = get_next_line(fd);
-	printf("--> %s\n\n", str);
-	free (str);
-	str = get_next_line(fd);
-	printf("--> %s\n\n", str);
-	free (str);
-	str = get_next_line(fd);
-	printf("--> %s\n\n", str);
-	free (str);
-	str = get_next_line(fd);
-	printf("--> %s\n\n", str);
-	free (str);
-	str = get_next_line(fd);
-	printf("--> %s\n\n", str);
-	free (str);
-	str = get_next_line(fd);
-	printf("--> %s\n\n", str);
-	free (str);
-	str = get_next_line(fd);
-	printf("--> %s\n\n", str);
-	free (str);
-	
-	// while (42)
-	// {
-	// 	str = get_next_line(fd);
-	// 	printf("--> %s\n", str);
-	// 	free (str);
-	// 	if (str == NULL)
-	// 		break ;
-	// }
+	// str = get_next_line(fd);
+	// printf("--> %s\n\n", str);
+	// free (str);
+	// str = get_next_line(fd);
+	// printf("--> %s\n\n", str);
+	// free (str);
+	// str = get_next_line(fd);
+	// printf("--> %s\n\n", str);
+	// free (str);
+	// str = get_next_line(fd);
+	// printf("--> %s\n\n", str);
+	// free (str);
+	// str = get_next_line(fd);
+	// printf("--> %s\n\n", str);
+	// free (str);
+	// str = get_next_line(fd);
+	// printf("--> %s\n\n", str);
+	// free (str);
+	// str = get_next_line(fd);
+	// printf("--> %s\n\n", str);
+	// free (str);
+	// str = get_next_line(fd);
+	// printf("--> %s\n\n", str);
+	// free (str);
+	// str = get_next_line(fd);
+	// printf("--> %s\n\n", str);
+	// free (str);
+	// str = get_next_line(fd);
+	// printf("--> %s\n\n", str);
+	// free (str);
+	// str = get_next_line(fd);
+	// printf("--> %s\n\n", str);
+	// free (str);
+	// str = get_next_line(fd);
+	// printf("--> %s\n\n", str);
+	// free (str);
+	while (42)
+	{
+		str = get_next_line(fd);
+		printf("--> %s\n", str);
+		free (str);
+		if (str == NULL)
+			break ;
+	}
 	close(fd);
 	return (0);
 }
